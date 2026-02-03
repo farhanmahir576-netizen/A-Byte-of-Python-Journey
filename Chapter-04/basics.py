1e20e6f9ccdd61c4ce6bf8505e5fd5ad9f1c135a
Farhan Azad Mahir <farhanmahir576@gmail.com>
2026-02-04 01:22:27 +0600

    Add basics.py for learning variables and formatting

---
+++ Chapter-04/basics.py
@@ -0,0 +1,10 @@
+# Chapter 4: Basics - Learning variables and format method
+age = 19
+name = 'Mahir'
+
+print('{0} is {1} years old'.format(name, age))
+print('Why is {0} playing with that python?'.format(name))
+
+# Practice with Escape Sequences
+print("This is the first line\nThis is the second line")
+print(r"Newlines are indicated by \n")
