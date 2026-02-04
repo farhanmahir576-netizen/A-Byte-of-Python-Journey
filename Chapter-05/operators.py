b2fad5a909059a084992016138ce6a7bac5c8f56
Farhan Azad Mahir <farhanmahir576@gmail.com>
2026-02-04 11:53:48 +0600

    Add operators and expressions example in operators.py

---
+++ Chapter-05/operators.py
@@ -0,0 +1,20 @@
+# Chapter 5: Operators and Expressions
+length = 5
+breadth = 2
+
+area = length * breadth
+print('Area is', area)
+print('Perimeter is', 2 * (length + breadth))
+
+# Logical and shortcut operators
+x = 2
+x = x + 3 # normal way
+print('x is', x)
+
+y = 10
+y += 3 # shortcut way (add and assign)
+print('y shortcut is', y)
+
+# Boolean evaluation
+is_equal = (area == 10)
+print('Is area equal to 10?', is_equal)
