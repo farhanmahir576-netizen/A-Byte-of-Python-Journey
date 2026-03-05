7dcef55ed0a4faf54ccdb3b64832403c053f6b13
Farhan Azad Mahir <farhanmahir576@gmail.com>
2026-03-05 20:10:34 +0600

    Update if_statement.py

--- Chapter-06/if_statement.py
+++ Chapter-06/if_statement.py
@@ -9,7 +9,6 @@ if guess == number:
 elif guess < number:
         # Another block
         print('No, it is a little higher than that')
-        # You can do whatever you want in a block...
 else: 
         print('No, it is a little lower than that')
         # you must have guessed > number to reach here
