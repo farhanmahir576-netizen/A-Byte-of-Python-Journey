0ae0da051ac99f7a3236c206a929e3868dec6a33
Farhan Azad Mahir <farhanmahir576@gmail.com>
2026-03-05 21:08:44 +0600

    Update and rename pickle_data.py to io_input.py

---
+++ Chapter-12/io_input.py
@@ -0,0 +1,10 @@
+def reverse (text):
+	return text[::-1]
+def is_palindrome(text):
+	return text == reverse(text)
+
+something = input ("Enter text:")
+if is_palindrome(something):
+	print("Yes, it is palindrome")
+else:
+	print("No, it is not palindrome")
